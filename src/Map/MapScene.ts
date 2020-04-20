2553d7b7e3ec96dd0db6e3106eefdcb66a382e6d
Leonardo Farroco <leonardo.farroco@gmail.com>
2020-04-19 21:20:44 -0300

    basic tile selection

--- src/Map/MapScene.ts
+++ src/Map/MapScene.ts
@@ -14,7 +14,10 @@ import {Unit} from '../Unit/Model';
 import BoardScene, {BOARD_SCENE_KEY} from '../Board/InteractiveBoardScene';
 import {INVALID_STATE} from '../errors';
 import button from '../UI/button';
-import {Container} from '../Models';
+import {Container, Image} from '../Models';
+import * as easyStar from 'easystarjs';
+
+const easystar = new easyStar.js();
 
 const cellSize = 100;
 
@@ -22,13 +25,13 @@ const boardPadding = 100;
 
 const Map: MapBoard = {
   cells: [
-    [0, 0, 0, 1, 1, 1],
+    [0, 1, 0, 1, 1, 1],
     [0, 1, 0, 1, 0, 1],
     [0, 1, 0, 0, 0, 1],
     [0, 0, 0, 1, 1, 1],
   ],
   forces: [
-    {id: '0', name: 'player', squads: [{id: '0', x: 1, y: 1}]},
+    {id: '0', name: 'player', squads: [{id: '0', x: 0, y: 0}]},
     {id: '1', name: 'CPU', squads: [{id: '3', x: 3, y: 2}]},
   ],
 };
@@ -47,6 +50,7 @@ type MapSquad = {
 export class MapScene extends Phaser.Scene {
   squads: Chara[] = [];
 
+  tiles: {x: number; y: number; type:number; tile: Image}[] = [];
   selectedUnit: string | null = null;
 
   constructor() {
@@ -55,14 +59,11 @@ export class MapScene extends Phaser.Scene {
 
   preload = preload;
 
-  getPos(x:number,y:number){
-
-  return{
-
-         x : boardPadding + x * cellSize,
-         y : boardPadding + y * cellSize
-  }
-
+  getPos(x: number, y: number) {
+    return {
+      x: boardPadding + x * cellSize,
+      y: boardPadding + y * cellSize,
+    };
   }
 
   renderMap(container: Container) {
@@ -70,13 +71,13 @@ export class MapScene extends Phaser.Scene {
 
     Map.cells.forEach((arr, col) =>
       arr.forEach((n, row) => {
-        const {x,y} = this.getPos(row,col)
+        const {x, y} = this.getPos(row, col);
         const tile = this.add.image(x, y, 'panel');
 
         tile.setInteractive();
         tile.on('pointerdown', () => {
           if (this.selectedUnit) {
-            this.moveUnit(this.selectedUnit, row, col);
+            this.selectTile(this.selectedUnit, row, col);
           }
         });
 
@@ -86,6 +87,13 @@ export class MapScene extends Phaser.Scene {
 
         tile.displayWidth = cellSize;
         tile.displayHeight = cellSize;
+
+        this.tiles.push({
+          x: row,
+          y: col,
+          type: n,
+          tile: tile,
+        });
       }),
     );
   }
@@ -127,6 +135,7 @@ export class MapScene extends Phaser.Scene {
       this.squads.forEach((c) => this.scene.remove('chara' + c.unit.id));
       container.destroy();
       this.squads = [];
+      this.tiles = [];
 
       this.scene.transition({
         target: 'TitleScene',
@@ -143,22 +152,54 @@ export class MapScene extends Phaser.Scene {
     this.renderUI(container);
   }
 
-  moveUnit(unitId: string, row: number, col: number) {
-    console.log(`MapScene - moveUnit`);
+  selectTile(unitId: string, row: number, col: number) {
+    console.log(`MapScene - selectTile`);
     const chara = this.squads.find((c) => c.unit.id === unitId);
+    const force = Map.forces.find((force) => force.id === '0');
 
-    if (!chara) throw new Error(INVALID_STATE);
+    if (!chara || !force) throw new Error(INVALID_STATE);
 
-    const {x,y} = this.getPos(row,col)
+    const charaPos = force.squads.find(
+      (squad) => squad.id === chara.unit.squad,
+    );
 
-    this.tweens.add({
-      targets: chara.container,
-      x: x,
-      y: y,
-      duration: 500,
-      onComplete: () => {
-        console.log(`combat!`);
-      },
+    if (!charaPos) throw new Error(INVALID_STATE);
+
+    easystar.setGrid(Map.cells);
+    easystar.setAcceptableTiles([0]);
+
+
+    this.tiles.forEach(tile=>tile.type === 0?tile.tile.clearTint(): null)
+
+    easystar.findPath(charaPos.x, charaPos.y, row, col, (path) => {
+
+      path.forEach((pos, index) => {
+
+        if(index === 0) return
+      const tile = this.getTileAt(pos.x,pos.y)
+
+      tile.tile.setTint(0x5555cc)
+        return
+        /**
+          if (index == 0) return;
+        const target = this.getPos(pos.x, pos.y);
+        this.tweens.add({
+          targets: chara.container,
+          x: target.x,
+          y: target.y,
+          duration: 500,
+          delay: (index - 1) * 500,
+        });
+        **/
+      });
     });
+    easystar.calculate();
+  }
+  getTileAt(x: number, y: number) {
+    const tile = this.tiles.find((tile) => tile.x === x && tile.y === y);
+
+    if (!tile) throw new Error(INVALID_STATE);
+
+    return tile;
   }
 }
