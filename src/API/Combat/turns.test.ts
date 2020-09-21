06cef98f3c297069ea7d093dad507a2604d960eb
Leonardo Farroco <leonardo.farroco@gmail.com>
2020-09-21 00:29:02 -0300

    fix tests

--- src/API/Combat/turns.test.ts
+++ src/API/Combat/turns.test.ts
@@ -9,10 +9,10 @@ function makeTurnUnit(unit: Unit) {
 
 test('Should sort by initiave correctly', () => {
   const units = [
-    {...makeUnit('fighter', 1, 1), dex: 9},
+    {...makeUnit('fighter', 0, 1), dex: 9},
     {...makeUnit('fighter', 1, 1), dex: 6},
-    {...makeUnit('fighter', 1, 1), dex: 7},
-    {...makeUnit('fighter', 1, 1), dex: 8},
+    {...makeUnit('fighter', 2, 1), dex: 7},
+    {...makeUnit('fighter', 3, 1), dex: 8},
   ].map(makeTurnUnit);
 
   const sorted = initiativeList(units).map((unit) => unit.unit.id);
@@ -22,10 +22,10 @@ test('Should sort by initiave correctly', () => {
 
 test('Combat should have the expected outcome', () => {
   const units = [
-    assignSquad(makeUnit('fighter', 1, 1), {id: '1', x: 1, y: 2}),
+    assignSquad(makeUnit('fighter', 0, 1), {id: '1', x: 1, y: 2}),
     assignSquad(makeUnit('fighter', 1, 1), {id: '1', x: 2, y: 2}),
-    assignSquad(makeUnit('fighter', 1, 1), {id: '1', x: 3, y: 2}),
-    assignSquad(makeUnit('fighter', 1, 1), {id: '2', x: 2, y: 2}),
+    assignSquad(makeUnit('fighter', 2, 1), {id: '1', x: 3, y: 2}),
+    assignSquad(makeUnit('fighter', 3, 1), {id: '2', x: 2, y: 2}),
   ]
 
   const res = runCombat(units);
