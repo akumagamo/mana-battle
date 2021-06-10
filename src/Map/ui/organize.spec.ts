00401b0ad96bddd13fed4846f49f2f2cd16d42a7
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-06-10 10:39:39 -0300

    adds specs to organize

---
+++ src/Map/ui/organize.spec.ts
@@ -0,0 +1,18 @@
+import * as MapScene from "../MapScene"
+import {organize} from "./organize"
+
+
+
+
+it.todo("should start ListSquadScene when clicked")
+
+it.todo("should stop MapScene when clicked")
+
+it.todo("should only pass units owned by the player to ListSquadScene")
+it.todo("should only pass squads owned by the player to ListSquadScene")
+it.todo("should call a fadeOut")
+it.todo("should pass the dispatched squads id to ListSquadScene")
+it.todo("should pass the dispatched squads id to ListSquadScene")
+it.todo("when the player returns, should merge the received data back into MapScene before starting it")
+it.todo("when the player returns, should stop ListSquadScene")
+it.todo("when the player returns, should clear ListSquadScene")
