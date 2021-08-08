19eb42d28c7dc13d58eec26120ce029a6d6e6e1f
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-08-08 20:54:57 -0300

    cleanup old assets

--- src/Chara/animations/animations.ts
+++ src/Chara/animations/animations.ts
@@ -1,48 +1,25 @@
 import { GAME_SPEED } from "../../env";
 
 export default (scene: Phaser.Scene) => {
-  scene.anims.create({
-    key: "stand",
-    frames: scene.anims.generateFrameNumbers("sprite_fighter", {
-      frames: [0, 1, 2],
-    }),
-    frameRate: 3 * GAME_SPEED,
-    repeat: -1,
-  });
+  loadAnimation(scene, "stand", [0, 1, 2]);
 
-  scene.anims.create({
-    key: "run",
-    frames: scene.anims.generateFrameNumbers("sprite_fighter", {
-      frames: [0, 1, 2],
-    }),
-    frameRate: 3 * GAME_SPEED,
-    repeat: -1,
-  });
+  loadAnimation(scene, "cast", [3, 4, 5]);
 
-  scene.anims.create({
-    key: "hit",
-    frames: scene.anims.generateFrameNumbers("sprite_fighter", {
-      frames: [0, 1, 2],
-    }),
-    frameRate: 3 * GAME_SPEED,
-    repeat: -1,
-  });
+  loadAnimation(scene, "run", [6, 7, 8]);
 
-  scene.anims.create({
-    key: "cast",
-    frames: scene.anims.generateFrameNumbers("sprite_fighter", {
-      frames: [0, 1, 2],
-    }),
-    frameRate: 3 * GAME_SPEED,
-    repeat: -1,
-  });
+  loadAnimation(scene, "hit", [9, 10, 11]);
 
-  scene.anims.create({
-    key: "die",
-    frames: scene.anims.generateFrameNumbers("sprite_fighter", {
-      frames: [0, 1, 2],
-    }),
-    frameRate: 3 * GAME_SPEED,
-    repeat: -1,
-  });
+  loadAnimation(scene, "die", [12, 13, 14]);
 };
+
+function loadAnimation(scene: Phaser.Scene, key: string, frames: number[]) {
+  if (!scene.anims.exists(key))
+    scene.anims.create({
+      key: key,
+      frames: scene.anims.generateFrameNumbers("sprite_fighter", {
+        frames,
+      }),
+      frameRate: 3 * GAME_SPEED,
+      repeat: -1,
+    });
+}
