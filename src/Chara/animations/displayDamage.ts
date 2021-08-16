6ce75354471ba47fe0f953b70728d01278dae9ac
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-08-15 21:42:38 -0300

    change damage display positoin

--- src/Chara/animations/displayDamage.ts
+++ src/Chara/animations/displayDamage.ts
@@ -1,22 +1,36 @@
-import {GAME_SPEED} from '../../env';
-import text from '../../UI/text';
-import {Chara} from '../Model';
+import { GAME_SPEED } from "../../env"
+import text from "../../UI/text"
+import { Chara } from "../Model"
+
+const TEXT_DAMAGE_POSITION_X = -20
+const TEXT_DAMAGE_POSITION_Y = -80
+const TEXT_DAMAGE_STROKE_COLOR = "#000000"
+const TEXT_DAMAGE_STROKE_WIDTH = 4
+const TEXT_DAMAGE_FADE_OUT_DURATION = 1500
+const TEXT_DAMAGE_FADE_OUT_Y_DISTANCE = -20
 
 export function displayDamage(chara: Chara, damage: number) {
-  const dmg = text(-20, -100, damage.toString(), chara.container, chara.scene);
-  dmg.setScale(2);
-  dmg.setShadow(2, 2, '#000');
-  dmg.setStroke('#000000', 4);
-  dmg.setAlign('center');
-  dmg.setDepth(Infinity);
+    const container = chara.scene.add.container()
+    const dmg = text(
+        chara.container.x + TEXT_DAMAGE_POSITION_X,
+        chara.container.y + TEXT_DAMAGE_POSITION_Y,
+        damage.toString(),
+        container,
+        chara.scene
+    )
+    dmg.setShadow(2, 2, "#000")
+    dmg.setStroke(TEXT_DAMAGE_STROKE_COLOR, TEXT_DAMAGE_STROKE_WIDTH)
+    dmg.setScale(2)
+    dmg.setAlign("center")
+    dmg.setFontStyle("bold")
+    dmg.setDepth(Infinity)
 
-  chara.scene.tweens.add({
-    targets: dmg,
-    y: dmg.y - 20,
-    alpha: 0,
-    duration: 1500 / GAME_SPEED,
-    onComplete: () => {
-      dmg.destroy();
-    },
-  });
+    chara.scene.tweens.add({
+        targets: dmg,
+        y: dmg.y + TEXT_DAMAGE_FADE_OUT_Y_DISTANCE,
+        duration: TEXT_DAMAGE_FADE_OUT_DURATION / GAME_SPEED,
+        onComplete: () => {
+            container.destroy()
+        },
+    })
 }
