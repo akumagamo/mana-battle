514d60d348b5ff159a229f9ace5894ce0e06c302
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-08-22 22:33:44 -0300

    rotate board depending on side

--- src/Combat/animations/fireball.ts
+++ src/Combat/animations/fireball.ts
@@ -1,43 +1,45 @@
-import { CharaIndex, getChara } from "../../Chara/Model";
-import fireball from "../../Chara/animations/spells/fireball";
-import { GAME_SPEED } from "../../env";
-import damageUnit from "./damageUnit";
-import { delay } from "../../Scenes/utils";
+import { CharaIndex, getChara } from "../../Chara/Model"
+import fireball from "../../Chara/animations/spells/fireball"
+import { GAME_SPEED } from "../../env"
+import damageUnit from "./damageUnit"
+import { delay } from "../../Scenes/utils"
+import { getSquad, isUnitInSquad, SquadIndex } from "../../Squad/Model"
+import { CombatBoardState } from "../Model"
 
 export default async function (
-  sourceId: string,
-  targetId: string,
-  newHp: number,
-  damage: number,
-  { scene, charaIndex }: { scene: Phaser.Scene; charaIndex: CharaIndex }
+    sourceId: string,
+    targetId: string,
+    newHp: number,
+    damage: number,
+    { scene, charaIndex, unitSquadIndex, left }: CombatBoardState
 ) {
-  const source = getChara(sourceId, charaIndex);
-  const target = getChara(targetId, charaIndex);
+    const source = getChara(sourceId, charaIndex)
+    const target = getChara(targetId, charaIndex)
 
-  const fireballSprite = fireball(
-    scene,
-    source.container.x,
-    source.container.y
-  );
+    const isLeft = isUnitInSquad(sourceId, left, unitSquadIndex)
 
-  fireballSprite.rotation = 1.9;
+    const fireballSprite = fireball(
+        scene,
+        source.container.x,
+        source.container.y
+    ).setRotation(1.9 * (isLeft ? -1 : 1))
 
-  if (process.env.SOUND_ENABLED) {
-    source.scene.sound.add("fireball").play();
-  }
+    if (process.env.SOUND_ENABLED) {
+        source.scene.sound.add("fireball").play()
+    }
 
-  scene.add.tween({
-    targets: fireballSprite,
-    x: target.container.x,
-    y: target.container.y,
-    duration: 700 / GAME_SPEED,
-    onComplete: () => {
-      fireballSprite.destroy();
-      damageUnit(target, newHp, damage);
-    },
-  });
+    scene.add.tween({
+        targets: fireballSprite,
+        x: target.container.x,
+        y: target.container.y,
+        duration: 700 / GAME_SPEED,
+        onComplete: () => {
+            fireballSprite.destroy()
+            damageUnit(target, newHp, damage)
+        },
+    })
 
-  source.cast();
-  await delay(scene, 1000 / GAME_SPEED);
-  source.stand();
+    source.cast()
+    await delay(scene, 1000 / GAME_SPEED)
+    source.stand()
 }
