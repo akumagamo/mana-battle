21619b0d226af5dd1e3b53938617bd153a89c15b
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-08-29 23:21:16 -0300

    prettier

--- src/Chara/animations/animations.ts
+++ src/Chara/animations/animations.ts
@@ -1,34 +1,63 @@
-import { GAME_SPEED } from "../../env";
+import { GAME_SPEED } from "../../env"
 
 export default (scene: Phaser.Scene) => {
-  loadAnimation(scene, "stand", [0, 1, 2]);
+    loadAnimation(scene, "stand", [0, 1, 2])
 
-  loadAnimation(scene, "cast", [3, 4, 5], false);
+    loadAnimation(scene, "cast", [3, 4, 5], false)
 
-  loadAnimation(scene, "run", [6, 7, 8]);
+    loadAnimation(scene, "run", [6, 7, 8])
 
-  loadAnimation(scene, "hit", [9, 10, 11], false);
+    loadAnimation(scene, "hit", [9, 10, 11], false)
 
-  loadAnimation(scene, "die", [12, 13, 14], false);
+    loadAnimation(scene, "die", [12, 13, 14], false)
 
-  loadAnimation(scene, "fireball", [0, 1, 2, 3, 4, 5, 6], false, 12, "fire");
-};
+    loadAnimation(scene, "fireball", [0, 1, 2, 3, 4, 5, 6], false, 12, "fire")
+
+    loadAtlasAnimation("fighter_atlas", "fighter_stand", scene)
+    loadAtlasAnimation("fighter_atlas", "fighter_run", scene)
+    loadAtlasAnimation("fighter_atlas", "fighter_cast", scene, true)
+    loadAtlasAnimation("fighter_atlas", "fighter_hit", scene, false, 0)
+}
 
 function loadAnimation(
-  scene: Phaser.Scene,
-  key: string,
-  frames: number[],
-  repeat = true,
-  frameRate = 3,
-  texture = "sprite_fighter"
+    scene: Phaser.Scene,
+    key: string,
+    frames: number[],
+    repeat = true,
+    frameRate = 3,
+    texture = "sprite_fighter"
 ) {
-  if (!scene.anims.exists(key))
+    if (!scene.anims.exists(key))
+        scene.anims.create({
+            key,
+            frames: scene.anims.generateFrameNumbers(texture, {
+                frames,
+            }),
+            frameRate: frameRate * GAME_SPEED,
+            repeat: repeat ? -1 : 0,
+        })
+}
+
+function loadAtlasAnimation(
+    atlasKey: string,
+    name: string,
+    scene: Phaser.Scene,
+    yoyo = true,
+    repeat = -1
+) {
+    if (scene.anims.exists(name)) return
+
+    const frames = scene.anims.generateFrameNames(atlasKey, {
+        prefix: `${name}_`,
+        suffix: ".png",
+        start: 0,
+        end: 12,
+        zeroPad: 2,
+    })
     scene.anims.create({
-      key,
-      frames: scene.anims.generateFrameNumbers(texture, {
-        frames,
-      }),
-      frameRate: frameRate * GAME_SPEED,
-      repeat: repeat ? -1 : 0,
-    });
+        key: name,
+        repeat,
+        yoyo,
+        frames: frames,
+    })
 }
