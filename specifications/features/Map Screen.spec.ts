2538dd9e91114b700b2c7d2c582b918c7e68013d
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-11-21 23:28:33 -0300

    refactor tests

--- specifications/features/Map Screen.spec.ts
+++ specifications/features/Map Screen.spec.ts
@@ -1,262 +1,288 @@
 import * as game from "../dsl"
 import "expect-puppeteer"
 import {
-  MapScreenProperties,
-  SquadIndex,
-  CityIndex,
-  createMapScreenProperties,
-  MapSceneState,
-  Squad
+    MapScreenProperties,
+    SquadIndex,
+    CityIndex,
+    createMapScreenProperties,
+    MapSceneState,
+    Squad,
 } from "../../modules/MapScene/Model"
 
 const port = process.env.CI ? 3333 : 3000
 
 beforeAll(async () => {
-  await page.goto(`http://localhost:${port}`)
-  await game.waitForSceneCreation(page, "Core Screen")
+    await page.goto(`http://localhost:${port}`)
+    await game.waitForSceneCreation(page, "Core Screen")
 })
 
 const defaultParameters = createMapScreenProperties({
-  squads: [
-    [100, 100, "PLAYER"],
-    [200, 200, "CPU"],
-  ],
-  cities: [
-    [50, 50, "PLAYER"],
-    [250, 250, "CPU"],
-  ],
+    squads: [
+        [100, 100, "PLAYER"],
+        [200, 200, "CPU"],
+    ],
+    cities: [
+        [50, 50, "PLAYER"],
+        [250, 250, "CPU"],
+    ],
 })
 
 describe("Map Screen", () => {
-
-  describe("Map Creation", () => {
-    openMapScreen(defaultParameters)
-    test("I should be in the Map Screen", assertCurrentScreen)
-    test("I have nothing selected", assertNothingSelected)
-    test("I should see the map", assertMapIsVisible)
-    test("I should see all squads", assertAllSquadsAreVisible)
-    test("I should see all cities", assertAllCitiesAreVisible)
-    test("Game is unpaused", assertGameIsUnpaused)
-  })
-
-  describe("Squad Selection", () => {
-    describe.each`
-      squadType   | canSeeEditOption 
-      ${"allied"} | ${true} 
-      ${"enemy"}  | ${false} 
-    `(
-      "$squadType squad selection", (
-        {squadType, canSeeEditOption}: {
-          squadType: ForceType, canSeeEditOption: boolean
-        }) => {
-
-      openMapScreen(defaultParameters);
-
-      test('When I have no squad selected', assertNoEntityIsSelected);
-
-      test(`When I select an ${squadType} squad`, selectSquadOfType(squadType));
-
-      test('Then I should see that the game is paused', assertGameIsPaused);
-
-      test(`Then I should see the View Squad Details option`,
-        assertOptionVisibilityInUI("View Squad Details Button", true)
-      )
-
-      test.skip(
-        `Then I ${should(canSeeEditOption)} see the View Move Squad option`,
-        assertOptionVisibilityInUI("Move Squad Button", canSeeEditOption)
-      )
-
+    describe("Map Creation", () => {
+        openMapScreen(defaultParameters)
+        test("When I am in in the Map Screen", assertCurrentScreen)
+        test("Then I have nothing selected", assertNothingSelected)
+        test("Then I should see the map", assertMapIsVisible)
+        test("Then I should see all squads", assertAllSquadsAreVisible)
+        test("Then I should see all cities", assertAllCitiesAreVisible)
+        test("Then I should see that the game is paused", assertGameIsUnpaused)
     })
 
-  })
+    describe("Squad Selection", () => {
+        describe.each`
+            squadType   | canSeeEditOption
+            ${"allied"} | ${true}
+            ${"enemy"}  | ${false}
+        `(
+            "$squadType squad selection",
+            ({
+                squadType,
+                canSeeEditOption,
+            }: {
+                squadType: ForceType
+                canSeeEditOption: boolean
+            }) => {
+                openMapScreen(defaultParameters)
 
-  describe("Open Squad Details", () => {
-    test.todo("User has opened Map Screen")
+                test("When I have no squad selected", assertNoEntityIsSelected)
 
-    test.todo("User has nothing selected")
+                test(
+                    `When I select an ${squadType} squad`,
+                    selectSquadOfType(squadType)
+                )
 
-    test.todo("/^User selects a x")
+                test(
+                    "Then I should see that the game is paused",
+                    assertGameIsPaused
+                )
 
-    test.todo("/^User selects option x")
+                test(
+                    `Then I should see the View Squad Details option`,
+                    assertOptionVisibilityInUI({
+                        optionName: "View Squad Details Button",
+                        shouldBeVisible: true,
+                    })
+                )
 
-    test.todo('/^Modal "(.*)" is visible$/')
-  })
+                const should_ = should(canSeeEditOption)
+                test.skip(
+                    `Then I ${should_} see the View Move Squad option`,
+                    assertOptionVisibilityInUI({
+                        optionName: "Move Squad Button",
+                        shouldBeVisible: canSeeEditOption,
+                    })
+                )
+            }
+        )
+    })
 
-  describe("Squad Movement", () => {
-    test.todo("User has opened Map Screen")
+    describe("Open Squad Details", () => {
+        test.todo("User has opened Map Screen")
 
-    test.todo("User has nothing selected")
+        test.todo("User has nothing selected")
 
-    test.todo("User selects a friendly squad")
+        test.todo("/^User selects a x")
 
-    test.todo('/^User selects option "(.*)"$/')
+        test.todo("/^User selects option x")
 
-    test.todo("User selects a location in the map")
+        test.todo('/^Modal "(.*)" is visible$/')
+    })
 
-    test.todo("Game is unpaused")
+    describe("Squad Movement", () => {
+        test.todo("User has opened Map Screen")
 
-    test.todo("Unit moves to that location")
-  })
+        test.todo("User has nothing selected")
 
-  describe("Squad Collision (friendly on enemy)", () => {
-    test.todo("User has opened Map Screen")
+        test.todo("User selects a friendly squad")
 
-    test.todo("User issued a move order to a squad")
+        test.todo('/^User selects option "(.*)"$/')
 
-    test.todo("Squad collides with enemy")
+        test.todo("User selects a location in the map")
 
-    test.todo('/^The "(.*)" modal is displayed$/')
-  })
+        test.todo("Game is unpaused")
 
-  describe("Squad Collision (enemy on friendly)", () => {
-    test.todo("User has opened Map Screen")
+        test.todo("Unit moves to that location")
+    })
 
-    test.todo("An enemy squad walks toward a friendly unit")
+    describe("Squad Collision (friendly on enemy)", () => {
+        test.todo("User has opened Map Screen")
 
-    test.todo("Enemy squad collides with friendly squad")
+        test.todo("User issued a move order to a squad")
 
-    test.todo('/^The "(.*)" modal is displayed$/')
-  })
+        test.todo("Squad collides with enemy")
 
-  describe("Enemy Encountered Modal", () => {
-    test.todo("User has opened Map Screen")
+        test.todo('/^The "(.*)" modal is displayed$/')
+    })
 
-    test.todo('/^The "(.*)" modal is opened$/')
+    describe("Squad Collision (enemy on friendly)", () => {
+        test.todo("User has opened Map Screen")
 
-    test.todo('/^User selects option "(.*)"$/')
+        test.todo("An enemy squad walks toward a friendly unit")
 
-    test.todo('/^The next screen should be "(.*)"$/')
-  })
+        test.todo("Enemy squad collides with friendly squad")
+
+        test.todo('/^The "(.*)" modal is displayed$/')
+    })
+
+    describe("Enemy Encountered Modal", () => {
+        test.todo("User has opened Map Screen")
+
+        test.todo('/^The "(.*)" modal is opened$/')
+
+        test.todo('/^User selects option "(.*)"$/')
+
+        test.todo('/^The next screen should be "(.*)"$/')
+    })
 })
 
 function should(condition: boolean) {
-  return condition ? 'should' : "shouldn't"
+    return condition ? "should" : "shouldn't"
 }
 
 function is(condition: boolean) {
-  return condition ? 'is' : "isn't"
+    return condition ? "is" : "isn't"
 }
 
-function assertOptionVisibilityInUI(optionName: string, shouldBeVisible: boolean) {
-  return async () => {
-    const visible = await page.evaluate(({optionName}) => {
-      const screenUI = window.game.scene.getScene("Map Screen UI")
-      const button = screenUI.children.getByName(optionName)
-      return Boolean(button)
-    }, {optionName})
+function assertOptionVisibilityInUI({
+    optionName,
+    shouldBeVisible,
+}: {
+    optionName: string
+    shouldBeVisible: boolean
+}) {
+    return async () => {
+        const visible = await page.evaluate(
+            ({ optionName }) => {
+                const screenUI = window.game.scene.getScene("Map Screen UI")
+                const button = screenUI.children.getByName(optionName)
+                return Boolean(button)
+            },
+            { optionName }
+        )
 
-    if (visible && !shouldBeVisible)
-      throw new Error(`Button "${optionName}" should not be visible`)
-    else if (!visible && shouldBeVisible)
-      throw new Error(`Button "${optionName}" should be visible`)
-  }
+        if (visible && !shouldBeVisible)
+            throw new Error(`Button "${optionName}" should not be visible`)
+        else if (!visible && shouldBeVisible)
+            throw new Error(`Button "${optionName}" should be visible`)
+    }
 }
 
 async function assertGameIsPaused() {
-  await page.evaluate(() => {
-    const scene = window.game.scene.getScene("Map Screen")
-    if (!scene.physics.world.isPaused) throw new Error("Game is not paused")
-  })
+    await page.evaluate(() => {
+        const scene = window.game.scene.getScene("Map Screen")
+        if (!scene.physics.world.isPaused) throw new Error("Game is not paused")
+    })
 }
 
 async function assertNoEntityIsSelected() {
-  await page.evaluate(() => {
-    const screenUI = window.game.scene.getScene("Map Screen UI")
-    const selectedUnitUI = screenUI.children.getByName("Selected Unit Info")
-    if (selectedUnitUI) throw new Error("There is a selected unit")
-  })
+    await page.evaluate(() => {
+        const screenUI = window.game.scene.getScene("Map Screen UI")
+        const selectedUnitUI = screenUI.children.getByName("Selected Unit Info")
+        if (selectedUnitUI) throw new Error("There is a selected unit")
+    })
 }
 
 async function assertGameIsUnpaused() {
-  await page.evaluate(() => {
-    const scene = window.game.scene.getScene("Map Screen")
-    if (scene.physics.world.isPaused) throw new Error("Game is paused")
-  })
+    await page.evaluate(() => {
+        const scene = window.game.scene.getScene("Map Screen")
+        if (scene.physics.world.isPaused) throw new Error("Game is paused")
+    })
 }
 
 async function assertAllCitiesAreVisible() {
-  await page.evaluate(() => {
-    const scene = window.game.scene.getScene("Map Screen")
-    const cities: CityIndex = scene.data.get("_state").cities
-    const allRendered = cities.every((city) =>
-      Boolean(scene.children.getByName(`city-${city.id.get("city")}`))
-    )
-    if (!allRendered)
-      throw new Error("Not all cities squads have been rendered")
-  })
+    await page.evaluate(() => {
+        const scene = window.game.scene.getScene("Map Screen")
+        const cities: CityIndex = scene.data.get("_state").cities
+        const allRendered = cities.every((city) =>
+            Boolean(scene.children.getByName(`city-${city.id.get("city")}`))
+        )
+        if (!allRendered)
+            throw new Error("Not all cities squads have been rendered")
+    })
 }
 
 async function assertAllSquadsAreVisible() {
-  await page.evaluate(() => {
-    const scene = window.game.scene.getScene("Map Screen")
-    const squads: SquadIndex = scene.data.get("_state").squads
+    await page.evaluate(() => {
+        const scene = window.game.scene.getScene("Map Screen")
+        const squads: SquadIndex = scene.data.get("_state").squads
 
-    const allRendered = squads.every((squad) =>
-      Boolean(scene.children.getByName(`squad-${squad.id.get("squad")}`))
-    )
+        const allRendered = squads.every((squad) =>
+            Boolean(scene.children.getByName(`squad-${squad.id.get("squad")}`))
+        )
 
-    if (!allRendered) throw new Error("Not all squads have been rendered")
-  })
+        if (!allRendered) throw new Error("Not all squads have been rendered")
+    })
 }
 
 async function assertMapIsVisible() {
-  const types = await page.evaluate(() => {
-    return window.game.scene
-      .getScene("Map Screen")
-      .children.list.map((child) => child.type)
-  })
+    const types = await page.evaluate(() => {
+        return window.game.scene
+            .getScene("Map Screen")
+            .children.list.map((child) => child.type)
+    })
 
-  expect(types).toContain("TilemapLayer")
+    expect(types).toContain("TilemapLayer")
 }
 
 async function assertNothingSelected() {
-  const selectedUnit = await game.getData(page, "Map Screen", "selectedUnit")
-  expect(selectedUnit).toBeUndefined()
+    const selectedUnit = await game.getData(page, "Map Screen", "selectedUnit")
+    expect(selectedUnit).toBeUndefined()
 }
 
 function openMapScreen(params: MapScreenProperties) {
-  beforeAll(async () => {
-    await page.evaluate((params) => {
-      window.game.scene.remove("Map Screen UI")
-      window.game.scene.remove("Map Screen")
-      window.game.events.emit("Start Map Screen", params)
-    }, params)
-  })
+    beforeAll(async () => {
+        await page.evaluate((params) => {
+            window.game.scene.remove("Map Screen UI")
+            window.game.scene.remove("Map Screen")
+            window.game.events.emit("Start Map Screen", params)
+        }, params)
+    })
 }
 
-type ForceType = "allied" | "enemy";
+type ForceType = "allied" | "enemy"
 function selectSquadOfType(squadType: ForceType) {
-  return async () => {
-    if (squadType === "allied") {
-      selectAnySquadFromForce("PLAYER")
-    } else if (squadType === "enemy") {
-      selectAnySquadFromForce("CPU")
-    } else throw new Error(`Invalid squad type: ${squadType}`)
-  }
+    return async () => {
+        if (squadType === "allied") {
+            selectAnySquadFromForce("PLAYER")
+        } else if (squadType === "enemy") {
+            selectAnySquadFromForce("CPU")
+        } else throw new Error(`Invalid squad type: ${squadType}`)
+    }
 }
 async function assertCurrentScreen() {
-  await game.currentScreenIs(page, "Map Screen")
+    await game.currentScreenIs(page, "Map Screen")
 }
 
 async function selectAnySquadFromForce(force: string) {
+    await page.evaluate(
+        ({ force }) => {
+            const scene = window.game.scene.getScene("Map Screen")
+            console.log(scene.data.get("_state"))
+            const first = (
+                scene.data.get("_state") as MapSceneState
+            ).squads.find((s: Squad) => s.force.get("force") === force)
 
-  await page.evaluate(({force}) => {
-    const scene = window.game.scene.getScene("Map Screen")
-    console.log(scene.data.get("_state"))
-    const first = (scene.data.get("_state") as MapSceneState)
-      .squads
-      .find((s: Squad) => s.force.get('force') === force)
+            if (!first) throw new Error("No squad for selection")
 
-    if (!first) throw new Error('No squad for selection')
+            const squadId = first.id.get("squad")
 
-    const squadId = first.id.get('squad')
+            if (!squadId) throw new Error("Invalid squad: no id")
 
-    if (!squadId) throw new Error('Invalid squad: no id')
+            const squad = scene.children.getByName(`squad-${squadId}`)
 
-    const squad = scene.children.getByName(`squad-${squadId}`)
-
-    if (!squad) throw new Error("Squad not created")
-    squad.emit("pointerup")
-  }, {force})
+            if (!squad) throw new Error("Squad not created")
+            squad.emit("pointerup")
+        },
+        { force }
+    )
 }
